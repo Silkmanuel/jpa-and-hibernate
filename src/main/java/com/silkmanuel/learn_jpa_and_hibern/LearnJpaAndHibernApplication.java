5290341c34d40ad916e7fd1ef7e30eef43c52ff0
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-08-03 11:19:37 +0100

    first commit

---
+++ src/main/java/com/silkmanuel/learn_jpa_and_hibern/LearnJpaAndHibernApplication.java
@@ -0,0 +1,13 @@
+package com.silkmanuel.learn_jpa_and_hibern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHibernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHibernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silkmanuel/learn_jpa_and_hibern/LearnJpaAndHibernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silkmanuel.learn_jpa_and_hibern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
